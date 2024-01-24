4e45d09a14a1ac2091f30802fb75273f82bb782b
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:33:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.IF_STATEMENT_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getmax(2, 10));
+            Console.ReadLine();
+        }
+        static int getmax(int num1, int num2)
+        {
+            if (num1 > num2) { return num1; 
+            }
+            else { return num2; }
+        }
+    }
+}
